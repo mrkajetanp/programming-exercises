1eb435acefad0c0b3bd7c02cfe95559f88947780
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-02-08 07:08:16 +0100

    Refactored and improved C++ AlternatingSplit.

--- C++/CodeWars/Cryptography/AlternatingSplit.cpp
+++ C++/CodeWars/Cryptography/AlternatingSplit.cpp
@@ -9,14 +9,15 @@ std::string encrypt (std::string text, int n) {
 	std::string temp1, temp2;
 
 	for (int i = 0; i < n; ++i) {
-		for (size_t i = 0, j = 0; i < str.size() ; ++i) {
-			if (j == 1) {
-				temp1.push_back (str[i]);
-				j = 0;
+		bool switcher = false;
+		for (auto c : str) {
+			if (switcher) {
+				temp1.push_back (c);
+				switcher = false;
 			}
 			else {
-				temp2.push_back (str[i]);
-				j++;
+				temp2.push_back (c);
+				switcher = true;
 			}
 		}
 		str = temp1+temp2;
@@ -33,25 +34,29 @@ std::string decrypt (std::string text, int n) {
 	std::string part1, part2;
 
 	for (int i = 0; i < n; ++i) {
-		std::cout << "s: " << str.size() << std::endl;
 		int split = (str.size()%2==0) ? (str.size()/2) : (str.size()-1)/2;
 		part1 = str.substr (0, split);
 		part2 = str.substr (split);
 		str.clear ();
 		for (size_t j = 0; j < part2.size(); ++j) {
 			str.push_back (part2[j]);
-			str.push_back (part1[j]);
+			if (part1[j])
+				str.push_back (part1[j]);
 		}
-		if (part1.size() != part2.size())
-			str.pop_back ();
 	}
 	return str;
 }
 
 int main () {
-	assert (decrypt("This is a test!", 0) == "This is a test!");
-	assert (decrypt("hsi  etTi sats!", 1) == "This is a test!");
-	assert (decrypt("s eT ashi tist!", 2) == "This is a test!");
-	assert (decrypt("This is a test!", 0) == "This is a test!");
+	assert (encrypt ("This is a test!", 0) == "This is a test!");
+	assert (encrypt ("This is a test!", 1) == "hsi  etTi sats!");
+	assert (encrypt ("This is a test!", 2) == "s eT ashi tist!");
+	assert (encrypt ("This is a test!", 3) == " Tah itse sits!");
+	assert (encrypt ("This is a test!", 4) == "This is a test!");
+	assert (encrypt ("This is a test!", -1) == "This is a test!");
+
+	assert (decrypt ("hsi  etTi sats!", 1) == "This is a test!");
+	assert (decrypt ("s eT ashi tist!", 2) == "This is a test!");
+	assert (decrypt ("This is a test!", 0) == "This is a test!");
 	return 0;
 }
