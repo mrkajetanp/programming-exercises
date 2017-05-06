8fa6708ec2c1175fcdec8bf96e39523b6d7ef2ac
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-05-06 20:06:41 +0200

    [Rust] Acronym (Exercism)

---
+++ Exercism/rust/acronym/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn abbreviate(input: &str) -> String {
+    let mut result = String::new();
+
+    for word in input.replace("-", " ").split_whitespace() {
+        if word.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_uppercase()) {
+            result.push(word.chars().nth(0).unwrap());
+        } else {
+            result.push(word.chars().nth(0).unwrap().to_uppercase().nth(0).unwrap());
+            for c in word.chars().skip(1).filter(|c| c.is_uppercase()) {
+                result.push(c);
+            }
+        }
+    }
+
+    result
+}
