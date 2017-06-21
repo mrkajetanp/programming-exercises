c50c9cb9e7acd4e972a8400f0c89ee5e852f0f57
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-06-21 22:26:32 +0200

    [Java] Grade School [Incomplete] (Exercism)

---
+++ Exercism/java/grade-school/src/main/java/School.java
@@ -0,0 +1,27 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by cajetan on 6/21/17.
+ */
+public class School {
+
+    public int numberOfStudents() {
+        return 0;
+    }
+
+    public void add(String name, int grade) {
+
+    }
+
+    public List<String> grade(int x) {
+        return Collections.singletonList("oo");
+    }
+
+    public Map<Integer, List<String>> studentsByGradeAlphabetical() {
+        return Collections.singletonMap(0, Collections.singletonList("o"));
+    }
+
+}
