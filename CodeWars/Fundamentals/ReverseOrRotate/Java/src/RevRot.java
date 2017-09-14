d35c48cae3b0219f73f0c1085ce21492f97d34b8
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-09-14 17:48:16 +0200

    [Java] Reverse or Rotate [Helper Functions] (CodeWars)

--- CodeWars/Fundamentals/ReverseOrRotate/Java/src/RevRot.java
+++ CodeWars/Fundamentals/ReverseOrRotate/Java/src/RevRot.java
@@ -1,7 +1,17 @@
 
 class RevRot {
 
-    public static String revRot(String strng, int sz) {
+    private static boolean ifReverse(String str) {
+        return str.chars()
+                .mapToDouble(c -> Math.pow(Character.getNumericValue(c), 3))
+                .sum()%2 == 0;
+    }
+
+    private static String rotate(String str) {
+        return str.substring(1) + str.charAt(0);
+    }
+
+    public static String revRot(String str, int sz) {
 
         return "";
     }
