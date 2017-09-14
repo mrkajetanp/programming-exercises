a806f9cc6e9f5995d1a81a00d06003111f9b87f5
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-09-14 17:58:10 +0200

    [Java] Reverse or Rotate (CodeWars)

--- CodeWars/Fundamentals/ReverseOrRotate/Java/src/RevRot.java
+++ CodeWars/Fundamentals/ReverseOrRotate/Java/src/RevRot.java
@@ -12,8 +12,26 @@ class RevRot {
     }
 
     public static String revRot(String str, int sz) {
+        if (sz <= 0 || sz > str.length())
+            return "";
 
-        return "";
+        StringBuilder result = new StringBuilder();
+
+        int idx = 0;
+        while (idx <= str.length()) {
+            String temp = str.substring(idx, idx+sz);
+
+            if (ifReverse(temp))
+                result.append(new StringBuilder(temp).reverse().toString());
+            else
+                result.append(rotate(temp));
+
+            idx += sz;
+            if (idx+sz > str.length())
+                break;
+        }
+
+        return result.toString();
     }
 
 }
