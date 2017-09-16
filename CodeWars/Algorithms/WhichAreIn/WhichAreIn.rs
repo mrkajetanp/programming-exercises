92241b624e89abdf3f636b0cb095eccbbbc5b549
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-09-16 19:14:21 +0200

    [Rust] Which are in? [Incomplete] (CodeWars)

--- CodeWars/Algorithms/WhichAreIn/WhichAreIn.rs
+++ CodeWars/Algorithms/WhichAreIn/WhichAreIn.rs
@@ -1,5 +1,6 @@
 
 fn in_array(arr1: Vec<&str>, arr2: Vec<&str>) -> Vec<String> {
+    Vec::new()
 }
 
 fn dotest(arr1: Vec<&str>, arr2: Vec<&str>, exp: Vec<&str>) {
@@ -10,4 +11,8 @@ fn main() {
     dotest(vec!["arp", "live", "strong"],
            vec!["lively", "alive", "harp", "sharp", "armstrong"],
            vec!["arp", "live", "strong"]);
+
+    dotest(vec!["cod", "code", "wars", "ewar", "pillow", "bed", "phht"],
+           vec!["lively", "alive", "harp", "sharp", "armstrong", "codewars", "cod", "code"],
+           vec!["cod", "code", "ewar", "wars"]);
 }
