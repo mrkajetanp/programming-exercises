e680a59c988f6e7dbb4a07e69f38697282fccad5
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-09-24 13:49:52 +0200

    [C] School Tally [More Helper Functions] (School)
    Student struct

--- Others/C/SchoolTally.c
+++ Others/C/SchoolTally.c
@@ -9,24 +9,34 @@ typedef struct _student {
     double avg;
 } Student;
 
-Student* new_student(char* name, char* surname, char* class, char* avg) {
+Student* new_student(char* name, char* surname, char* class, double avg) {
     Student* result = malloc(sizeof(Student));
 
     result->name = malloc(strlen(name+1));
     strcpy(result->name, name);
+
     result->surname = malloc(strlen(surname+1));
     strcpy(result->surname, surname);
+
     result->class = malloc(strlen(class+1));
     strcpy(result->class, class);
-    result->avg = atof(avg);
+
+    result->avg = avg;
 
     return result;
 }
 
-void print_student_list(FILE* file) {
-    char line[256];
+void free_student(Student* student) {
+    free(student->name);
+    free(student->surname);
+    free(student->class);
+    free(student);
+}
 
-    int lines = 0;
+void read_student_array(Student** arr, FILE* file, int* size) {
+    *size = 0;
+
+    char line[256];
     while (fgets(line, sizeof(line), file)) {
         line[strlen(line)-1] = '\0';
         char* name = strtok(line, " ");
@@ -34,17 +44,33 @@ void print_student_list(FILE* file) {
         char* class = strtok(NULL, " ");
         char* avg = strtok(NULL, " ");
 
-        printf("%s %s, %s - %s\n", name, surname, class, avg);
+        arr[(*size)++] = new_student(name, surname, class, atof(avg));
     }
+}
 
-    printf("\nlines: %d \n", lines);
+void free_student_array(Student** arr, int size) {
+    for (int i = 0 ; i < size ; ++i) {
+        free_student(arr[i]);
+    }
+}
+
+void print_student_array(Student** arr, int size) {
+    for (int i = 0 ; i < size ; ++i) {
+        printf("%s %s, %s - %f\n", arr[i]->name, arr[i]->surname,
+               arr[i]->class, arr[i]->avg);
+    }
 }
 
 int main() {
     FILE* input_file = fopen("szkola.txt", "r");
 
-    print_student_list(input_file);
+    Student* arr[500];
+    int size = 0;
+    read_student_array(arr, input_file, &size);
 
+    print_student_array(arr, size);
+
+    free_student_array(arr, size);
     fclose(input_file);
     return 0;
 }
