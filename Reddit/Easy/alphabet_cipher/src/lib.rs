4ca94ddfff86311af391d0ad3837afe5bcd026e7
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-04-15 04:20:13 +0200

    [Rust] Alphabet Cipher - Encrypt implementation (Reddit)

--- Reddit/Easy/alphabet_cipher/src/lib.rs
+++ Reddit/Easy/alphabet_cipher/src/lib.rs
@@ -1,21 +1,12 @@
 
 pub fn encrypt(message: &str, keyword: &str) -> String {
-    println!("encrypt {} with {}", message, keyword);
-
     let message: Vec<char> = message.chars().collect();
     let keyword: Vec<char> = keyword.chars().collect();
 
-    let mut result = String::new();
-
-    for i in 0..message.len() {
-        // println!("{} with {} -> {}", message[i],
-                 // keyword[i%keyword.len()],
-                 // (97 + ((keyword[i%keyword.len()] as u8 - 97u8 + message[i] as u8 - 97u8)%26u8)) as char);
-
-        result.push((97 + ((keyword[i%keyword.len()] as u8 - 97u8 + message[i] as u8 - 97u8)%26u8)) as char);
-    }
-
-    result
+    (0..message.len())
+        .map(|i| (97 + ((keyword[i%keyword.len()] as u8 +
+                         message[i] as u8 - 194u8)%26u8)) as char)
+        .collect()
 }
 
 pub fn decrypt(code: &str, keyword: &str) -> String {
