000a80c55e7c0194a56c8230ee29c24b94430d2b
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-05-03 18:46:55 +0200

    [Python] Reverse or Rotate - Final solution (CodeWars)

--- CodeWars/Fundamentals/ReverseOrRotate/ReverseOrRotate.py
+++ CodeWars/Fundamentals/ReverseOrRotate/ReverseOrRotate.py
@@ -1,18 +1,20 @@
 import unittest
 
-def if_reverse(s):
-    return sum([int(c)**3 for c in s])%2 == 0
+def process(s, size):
+    if len(s) != size:
+        return ""
+    elif sum([int(c)**3 for c in s])%2 == 0:
+        return s[::-1]
+    else:
+        return s[1::]+s[0]
 
-def rotate(s):
-    return s[1::]+s[0]
 
-def revrot(string, size):
-    if size <= 0 or size > len(string) or not string:
+def revrot(s, size):
+    if size <= 0 or size > len(s) or not s:
         return ""
 
-    print([string[i:i+size] for i in range(0, len(string), size)])
-    print("".join(["ab", "cd"]))
-    return ""
+    return "".join([process(s[i:i+size], size) for i in range(0, len(s), size)])
+
 
 class TestExercise(unittest.TestCase):
     def test_basic(self):
@@ -22,8 +24,8 @@ class TestExercise(unittest.TestCase):
         s = "733049910872815764"
         a = "330479108928157"
         self.assertEqual(revrot(s, 5), a)
-        s = 73304991087281576455176044327690580265896
-        a = 1994033775182780067155464327690480265895
+        s = "73304991087281576455176044327690580265896"
+        a = "1994033775182780067155464327690480265895"
         self.assertEqual(revrot(s, 8), a)
 
 if __name__ == '__main__':
