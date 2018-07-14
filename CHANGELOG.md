0af2d7e0c08fe3802f65194b9afab273f7f6d3fa
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-07-14 13:01:01 +0200

    [Rust] Code of Kutulu - Initial file (CodinGame)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ---
 
+#### 14.07.2018
+
+* [Rust] Code of Kutulu - Initial file (CodinGame)
+
 #### 13.07.2018
 
 * [Rust] CodeRoyale - General refactoring adujustments (CodinGame)
---
+++ CodinGame/CodeRoyale/CodeOfKutulu.rs
@@ -0,0 +1,67 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Survive the wrath of Kutulu
+ * Coded fearlessly by JohnnyYuge & nmahoude
+(ok we might have been a bit scared by the old god...but don't say anything)
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+
+    let width = parse_input!(input_line, i32);
+
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+
+    let height = parse_input!(input_line, i32);
+
+    for i in 0..height as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let line = input_line.trim_right().to_string();
+        let mut input_line = String::new();
+
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        // how much sanity you lose every turn when alone, always 3 until wood 1
+        let sanity_loss_lonely = parse_input!(inputs[0], i32);
+        // how much sanity you lose every turn when near another player
+        // always 1 until wood 1
+        let sanity_loss_group = parse_input!(inputs[1], i32);
+        // how many turns the wanderer take to spawn, always 3 until wood 1
+        let wanderer_spawn_time = parse_input!(inputs[2], i32);
+        // how many turns the wanderer is on map after spawning
+        // always 40 until wood 1
+        let wanderer_life_time = parse_input!(inputs[3], i32);
+
+        // game loop
+        loop {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            // the first given entity corresponds to your explorer
+            let entity_count = parse_input!(input_line, i32);
+            for i in 0..entity_count as usize {
+                let mut input_line = String::new();
+                io::stdin().read_line(&mut input_line).unwrap();
+                let inputs = input_line.split(" ").collect::<Vec<_>>();
+                let entity_type = inputs[0].trim().to_string();
+                let id = parse_input!(inputs[1], i32);
+                let x = parse_input!(inputs[2], i32);
+                let y = parse_input!(inputs[3], i32);
+                let param_0 = parse_input!(inputs[4], i32);
+                let param_1 = parse_input!(inputs[5], i32);
+                let param_2 = parse_input!(inputs[6], i32);
+            }
+
+            // Write an action using println!("message...");
+            // To debug: eprintln!("Debug message...");
+
+            println!("WAIT"); // MOVE <x> <y> | WAIT
+        }
+    }
+}
