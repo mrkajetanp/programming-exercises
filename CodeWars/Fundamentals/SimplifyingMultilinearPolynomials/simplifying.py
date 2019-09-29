fcaccca4a4bf573fffb1c3167a590a86daadf5a5
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-29 13:22:51 +0100

    [Python] CodeWars - Simplifying (Sorting Variables)

--- CodeWars/Fundamentals/SimplifyingMultilinearPolynomials/simplifying.py
+++ CodeWars/Fundamentals/SimplifyingMultilinearPolynomials/simplifying.py
@@ -3,13 +3,14 @@ def simplify(poly):
     import re
 
     matches = re.findall(r'([+\-]?)(\d*)([a-z]+)', poly)
-
     print(matches)
 
     expanded = [[int(i[0] + (i[1] if i[1] != "" else "1")),
                  ''.join(sorted(i[2]))] for i in matches]
-
     print(expanded)
+
+    variables = sorted(list(set(i[1] for i in expanded)), key=lambda x: (len(x), x))
+    print(variables)
 
     return poly
 
@@ -28,8 +29,6 @@ assert(simplify("xzy+zby") == "byz+xyz")
 assert(simplify("-y+x") == "x-y")
 assert(simplify("y-x") == "-x+y")
 
-    # get the unique variables from above list. Sort them first by length, then alphabetically
-    # variables = sorted(list(set(i[1] for i in expanded)), key=lambda x: (len(x), x))
     # get the sum of coefficients (located in expanded) for each variable
     # coefficients = {v:sum(i[0] for i in expanded if i[1] == v) for v in variables}
     # clean-up: join them with + signs, remove '1' coefficients, and change '+-' to '-'
