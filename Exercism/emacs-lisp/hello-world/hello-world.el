112161c24c1bd03208ed8d5c9a19477448f3b3f8
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-04-03 16:02:04 +0100

    [Exercism] Emacs Lisp - Hello World

---
+++ Exercism/emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,13 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'hello-world)
+
+(defun hello ()
+  "Hello, World!"
+)
+;;; hello-world.el ends here
