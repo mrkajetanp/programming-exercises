02018cb32555df5512dd0b5c33afd48e844b512a
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-05-07 18:10:53 +0100

    [Exercism] C - Sum of Multiples Adjustment

--- Exercism/c/sum-of-multiples/src/sum_of_multiples.c
+++ Exercism/c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,34 +1,30 @@
-#include <stdbool.h>
 #include "sum_of_multiples.h"
+#include <stdbool.h>
 
-bool arr_contains(int* arr, int size, int x) {
-    bool contains = false;
-
-    for (int i = 0 ; i < size ; ++i) {
-        if (arr[i] == x) {
-            contains = true;
-            break;
-        }
+bool arr_contains(int *arr, int size, int x) {
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] == x) {
+      return true;
     }
+  }
 
-    return contains;
+  return false;
 }
 
-long sum_of_multiples(const unsigned int* multiples, int size, int num) {
-    int vec[1000];
-    int vec_size = 0;
+long sum_of_multiples(const unsigned int *multiples, int size, int num) {
+  int vec[1000];
+  int vec_size = 0;
 
-    for (int i = 0 ; i < size ; ++i) {
-        for (int j = 1 ; j < num ; ++j) {
-            if (j%multiples[i] == 0 && !arr_contains(vec, vec_size, j))
-                vec[vec_size++] = j;
-        }
+  for (int i = 0; i < size; ++i) {
+    for (int j = 1; j < num; ++j) {
+      if (j % multiples[i] == 0 && !arr_contains(vec, vec_size, j))
+        vec[vec_size++] = j;
     }
+  }
 
-    long sum = 0;
+  long sum = 0;
+  for (int i = 0; i < vec_size; ++i)
+    sum += vec[i];
 
-    for (int i = 0 ; i < vec_size ; ++i)
-        sum += vec[i];
-
-    return sum;
+  return sum;
 }
