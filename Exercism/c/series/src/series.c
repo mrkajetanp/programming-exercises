5a5dae45505f85bb42f55400ad35245725c1965b
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-05-12 21:06:10 +0100

    [Exercism] C - Two Fer

--- Exercism/c/series/src/series.c
+++ Exercism/c/series/src/series.c
@@ -1,27 +1,26 @@
+#include "series.h"
 #include <stdlib.h>
 #include <string.h>
-#include "series.h"
 
 SeriesResults_t series(char *inputText, unsigned int substringLength) {
-    SeriesResults_t* result = malloc(sizeof result);
-    result->substringCount = 0;
+  SeriesResults_t *result = malloc(sizeof result);
+  result->substringCount = 0;
 
-    unsigned int input_len = strlen(inputText);
-    if (substringLength > input_len || substringLength == 0)
-        return *result;
-
-    result->substring = malloc(sizeof(char*) * input_len-substringLength+1);
-
-
-    int idx = 0;
-
-    while (idx+substringLength <= input_len) {
-        result->substring[result->substringCount] = malloc(substringLength+1);
-        strncpy(result->substring[result->substringCount++],
-                inputText+idx, substringLength);
-
-        idx++;
-    }
-
+  unsigned int input_len = strlen(inputText);
+  if (substringLength > input_len || substringLength == 0)
     return *result;
+
+  result->substring = malloc(sizeof(char *) * input_len - substringLength + 1);
+
+  int idx = 0;
+
+  while (idx + substringLength <= input_len) {
+    result->substring[result->substringCount] = malloc(substringLength + 1);
+    strncpy(result->substring[result->substringCount++], inputText + idx,
+            substringLength);
+
+    idx++;
+  }
+
+  return *result;
 }
