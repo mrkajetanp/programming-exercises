5851d9dda39cc0b1a564e3640fca9d62fa85be6b
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-05-17 21:28:16 +0100

    [Exercism] Ruby - Scrabble Score

---
+++ Exercism/ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Scrabble ...
+class Scrabble
+  def initialize(input)
+    @score = self.class.compute_score(input)
+  end
+
+  def self.score(input)
+    compute_score(input)
+  end
+
+  def score
+    @score
+  end
+
+  def self.compute_score(input)
+    return 0 if input.nil?
+
+    scores = {
+      'a' => 1, 'e' => 1, 'i' => 1, 'o' => 1, 'u' => 1, 'l' => 1,
+      'n' => 1, 'r' => 1, 's' => 1, 't' => 1, 'd' => 2, 'g' => 2,
+      'b' => 3, 'c' => 3, 'm' => 3, 'p' => 3, 'f' => 4, 'h' => 4,
+      'v' => 4, 'w' => 4, 'y' => 4, 'k' => 5, 'j' => 8, 'x' => 8,
+      'q' => 10, 'z' => 10
+    }
+
+    input.strip.downcase.chars.reduce(0) { |sum, c| sum + scores[c] }
+  end
+end
