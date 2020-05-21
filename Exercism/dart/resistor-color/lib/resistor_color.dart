c485733aec278a4064af94ee9add7204918eed3f
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-05-21 15:34:22 +0100

    [Exercism] Dart - Resistor Color

---
+++ Exercism/dart/resistor-color/lib/resistor_color.dart
@@ -0,0 +1,10 @@
+class ResistorColor {
+    var colors = [
+      "black", "brown", "red", "orange", "yellow",
+      "green", "blue", "violet", "grey", "white"
+    ];
+
+    int colorCode(String color) {
+      return colors.indexOf(color);
+    }
+}
