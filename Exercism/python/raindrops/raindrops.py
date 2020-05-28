8a6b72fc4ef5d1f84a246cfeade6335f9564e7ee
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-05-28 21:26:23 +0100

    [Python] Exercism - Raindrops

---
+++ Exercism/python/raindrops/raindrops.py
@@ -0,0 +1,17 @@
+
+def convert(number):
+    result = ""
+
+    if number%3 == 0:
+        result += "Pling"
+
+    if number%5 == 0:
+            result += "Plang"
+
+    if number%7 == 0:
+            result += "Plong"
+
+    if not len(result):
+        result += str(number)
+
+    return result
