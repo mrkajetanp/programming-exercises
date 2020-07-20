4a2fee0ebde66d3d3056599d6a15e60667e9f096
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-07-20 17:00:00 +0100

    [Python] Exercism - Grains

---
+++ Exercism/python/grains/grains.py
@@ -0,0 +1,7 @@
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("Number has to be between 1 and 64")
+    return 2 ** (number-1)
+
+def total():
+    return 2 ** 64 - 1
