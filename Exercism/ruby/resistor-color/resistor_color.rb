fd150de75e03dcd7ec133abb684d3f9a7ee8d961
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-07-24 17:00:00 +0100

    [Exercism] Ruby - Resistor Color

---
+++ Exercism/ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# ResistorColor ...
+class ResistorColor
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+
+  def self.color_code(color)
+    COLORS.index(color)
+  end
+end
