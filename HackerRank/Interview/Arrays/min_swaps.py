aba709c5949fac9dcbe25920bf7e78cdf9113461
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-11-11 23:02:24 +0000

    [HackerRank] Interview - Arrays - Min Swaps

--- HackerRank/Interview/Arrays/min_swaps.py
+++ HackerRank/Interview/Arrays/min_swaps.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python3
 
 def minimumSwaps(arr):
-    result = 0
-    indices = {v: i for i, v in enumerate(arr)}
-    sorted_arr = sorted(arr)
+    swaps = 0
 
-    for i in range(len(arr)):
-        if arr[i] == sorted_arr[i]:
-            continue
+    val_idx = dict(zip(arr, range(1, len(arr) + 1)))
+    print(val_idx)
 
-        idx_to_swap = indices[sorted_arr[i]]
-        arr[i], arr[idx_to_swap] = arr[idx_to_swap], arr[i]
-        indices[arr[i]] = idx_to_swap
-        indices[sorted_arr[i]] = i
-        result += 1
+    for i in range(1, len(arr) + 1):
+        if val_idx[i] != i:
+            val_idx[arr[i-1]] = val_idx[i]
+            arr[val_idx[i]-1] = arr[i-1]
+            swaps += 1
 
-    return result
-
-    #         to_swap_ix = index_dict[correct_value]
-    #         arr[to_swap_ix],arr[i] = arr[i], arr[to_swap_ix]
-    #         index_dict[v] = to_swap_ix
-    #         index_dict[correct_value] = i
-    #         swaps += 1
+    return swaps
 
 print(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
 print(minimumSwaps([4, 3, 1, 2]))
 print(minimumSwaps([2, 3, 4, 1, 5]))
 print(minimumSwaps([1, 3, 5, 2, 4, 6, 7]))
-print(minimumSwaps([]))
-print(minimumSwaps([2, 1]))
