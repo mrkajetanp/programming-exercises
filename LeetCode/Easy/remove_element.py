c0b0c2096c888220ffad56261af889dc9baeed2b
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-12-08 18:41:18 +0000

    [LeetCode] Easy - Remove Element

---
+++ LeetCode/Easy/remove_element.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def removeElement(nums, val: int) -> int:
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != val:
+            nums[i] = nums[j]
+            i += 1
+    return i
